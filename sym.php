4e269170ad0feb8efafd5125b65cd5f6d9b262dd
Amit Kumar <amitsarajput@gmail.com>
2025-06-20 12:38:49 +0530

    staging set to git 1.00

--- sym.php
+++ sym.php
@@ -1,8 +1,5 @@
 <?php
-// $target = '/home/identity2000/Parked-Domains/radartyres.com/staging/laravel-core/storage/app/public';
-// $shortcut = 'storage';
-// symlink($target, $shortcut);
-$target = '/laravel-core/storage/app/public';
+$target = '/opt/1panel/apps/openresty/openresty/www/sites/staging.radartires.com/index/laravel-core/storage/app/public';
 $shortcut = 'storage';
 symlink($target, $shortcut);
 ?>
